b49d9059f22c16aa8a9bf420b3cfd357a2b770e2
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-10-28 15:53:55 +0400

    validateRegister name field fix

--- utils/validators/userValidator.js
+++ utils/validators/userValidator.js
@@ -12,7 +12,7 @@ module.exports.validateRegister = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(URL_REGEXP),
   }),
